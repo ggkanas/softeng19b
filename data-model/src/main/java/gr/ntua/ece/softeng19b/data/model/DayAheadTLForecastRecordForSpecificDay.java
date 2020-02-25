a0be29d62a3e18b9e8a74675441bc76aa423e51a
fotinila <58375242+fotinila@users.noreply.github.com>
2020-02-25 15:30:45 +0100

    Create DayAheadTLForecastRecordForSpecificDay.java

---
+++ data-model/src/main/java/gr/ntua/ece/softeng19b/data/model/DayAheadTLForecastRecordForSpecificDay.java
@@ -0,0 +1,45 @@
+package gr.ntua.ece.softeng19b.data.model;
+
+public class DayAheadTLForecastRecordForSpecificDay extends AbstractEntsoeRecord {
+
+    private int day;
+    private DateTime dateTime; 
+    private double dayAheadTotalLoadValue;
+    private DateTime UpdateTime;
+
+    public DayAheadTotalLoadForecastRecordForSpecificDay() {
+        super(DataSet.dayAheadTotalLoad);
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public double getDayAheadTotalLoadForecastValue() {
+        return actualTotalLoadForecastValue;
+    }
+
+    public DateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(DateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public DateTime getUpdateTime() {
+        return UpdateTime;
+    }
+
+    public void setUpdateTime(DateTime UpdateTime) {
+        this.UpdateTime = UpdateTime;
+    }
+
+    public void setDayAheadTotalLoadForecastValue(double actualTotalLoadForecastValue) {
+        this.actualTotalLoadForecastValue = actualTotalLoadForecastValue;
+    }
+}
