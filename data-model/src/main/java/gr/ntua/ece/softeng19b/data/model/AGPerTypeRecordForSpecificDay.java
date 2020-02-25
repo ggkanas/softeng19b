f9ab2469e26dd0a373d0200bccc48516e86027a9
ElenaVerg <58372812+ElenaVerg@users.noreply.github.com>
2020-02-25 17:28:45 +0200

    Create AGPerTypeRecordForSpecificDay.java

---
+++ data-model/src/main/java/gr/ntua/ece/softeng19b/data/model/AGPerTypeRecordForSpecificDay.java
@@ -0,0 +1,45 @@
+package gr.ntua.ece.softeng19b.data.model;
+
+public class AGPerTypeRecordForSpecificDay extends AbstractEntsoeRecord {
+
+    private int day;
+    private DateTime dateTime; //*** NEW ***
+    private double actualGenerationOutputValue;
+    private DateTime UpdateTime;
+
+    public AGPerTypeRecordForSpecificDay() {
+        super(DataSet.ActualGenerationOutput);
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public double getactualGenerationOutputValue() {
+        return actualGenerationOutputValue;
+    }
+    //*** NEW ***
+    public DateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(DateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public DateTime getUpdateTime() {
+        return UpdateTime;
+    }
+
+    public void setUpdateTime(DateTime UpdateTime) {
+        this.UpdateTime = UpdateTime;
+    }
+
+    public void setactualGenerationOutputValue(double actualGenerationOutputValue) {
+        this.actualGenerationOutputValue = actualGenerationOutputValue;
+    }
+}
