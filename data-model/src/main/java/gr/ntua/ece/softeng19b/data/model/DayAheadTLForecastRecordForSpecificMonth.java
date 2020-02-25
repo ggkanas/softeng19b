1627335be750e7eb58b1c65c9d3d2ed74ec9e4f9
ElenaVerg <58372812+ElenaVerg@users.noreply.github.com>
2020-02-25 16:56:22 +0200

    Create DayAheadTLForecastRecordForSpecificMonth.java

---
+++ data-model/src/main/java/gr/ntua/ece/softeng19b/data/model/DayAheadTLForecastRecordForSpecificMonth.java
@@ -0,0 +1,28 @@
+package gr.ntua.ece.softeng19b.data.model;
+
+public class DayAheadTLForecastRecordForSpecificMonth extends AbstractEntsoeRecord {
+
+    private int month;
+    private double monthAheadTotalLoadForecastByDayValue;
+
+    public DayAheadTotalLoadForecastRecordForSpecificMonth() {
+        super(DataSet.monthAheadTotalLoadByDay);
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public double getDayAheadTLForecastByDayValue() {
+        return DayAheadTLForecastByDayValue;
+    }
+
+
+    public void setDayAheadTLForecastByDayValue(double DayAheadTLForecastByDayValueValue) {
+        this.DayAheadTLForecastByDayValue = DayAheadTLForecastByDayValue;
+    }
+}
