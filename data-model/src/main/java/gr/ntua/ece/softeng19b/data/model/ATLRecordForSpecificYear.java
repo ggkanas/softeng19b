a8de9e6fe930e4950a39ea0a8e996784143a6154
fotinila <58375242+fotinila@users.noreply.github.com>
2020-02-25 15:40:35 +0100

    Update ATLRecordForSpecificYear.java

--- data-model/src/main/java/gr/ntua/ece/softeng19b/data/model/ATLRecordForSpecificYear.java
+++ data-model/src/main/java/gr/ntua/ece/softeng19b/data/model/ATLRecordForSpecificYear.java
@@ -2,7 +2,17 @@ package gr.ntua.ece.softeng19b.data.model;
 
 public class ATLRecordForSpecificYear extends AbstractEntsoeRecord {
 
+    private int year; // *** NEW ***
     private double actualDataLoadByMonthValue;
+    
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
 
     public ATLRecordForSpecificYear() {
         super(DataSet.ActualTotalLoad);
