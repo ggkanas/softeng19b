b1065bb856a12f04c7b8f46702b4f9a5fcd1ceb3
fotinila <58375242+fotinila@users.noreply.github.com>
2020-02-25 16:31:35 +0100

    Create AggregatedGenerationPerTypeRecordForSpecificMonth.java

---
+++ data-model/src/main/java/gr/ntua/ece/softeng19b/data/model/AggregatedGenerationPerTypeRecordForSpecificMonth.java
@@ -0,0 +1,19 @@
+package gr.ntua.ece.softeng19b.data.model;
+
+public class AggregatedGenerationPerTypeRecordForSpecificMonth extends AbstractEntsoeRecord {
+
+    private int month;
+    private double actualGenerationOutputByDayValue;
+
+    public AggregatedGenerationPerTypeRecordForSpecificMonth() {
+        super(DataSet.actualGenerationOutputByDayValue);
+    }
+   
+   public double actualGenerationOutputByDayValue() {
+        return actualGenerationOutputByDayValue;
+    }
+  
+    public void setAggregatedGenerationPerTypeRecordForSpecificMonth (double actualGenerationOutputByDayValue) {
+        this.actualGenerationOutputByDayValue = actualGenerationOutputByDayValue;
+    }
+}
