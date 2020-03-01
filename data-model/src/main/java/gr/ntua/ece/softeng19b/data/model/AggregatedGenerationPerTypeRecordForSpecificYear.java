0df6196f34c2b3324e6193ece96b37a2b1b9be03
a-panagiotic <58482696+a-panagiotic@users.noreply.github.com>
2020-03-01 19:58:47 +0200

    Update AggregatedGenerationPerTypeRecordForSpecificYear.java

--- data-model/src/main/java/gr/ntua/ece/softeng19b/data/model/AggregatedGenerationPerTypeRecordForSpecificYear.java
+++ data-model/src/main/java/gr/ntua/ece/softeng19b/data/model/AggregatedGenerationPerTypeRecordForSpecificYear.java
@@ -4,11 +4,20 @@ public class AggregatedGenerationPerTypeRecordForSpecificYear extends AbstractEn
 
     private int year;
     private double actualGenerationOutputByMonthValue;
-
+    private String productionType;
+    
     public AggregatedGenerationPerTypeRecordForSpecificYear () {
         super(DataSet.actualGenerationOutputByMonth);
     }
    
+    public String getProductionType() {
+        return productionType;
+    }
+    
+    public void setProductionType(String productionType) {
+        this.productionType = productionType;
+    }    
+    
    public double actualGenerationOutputByMonthValue() {
         return actualGenerationOutputByMonthValue;
     }
