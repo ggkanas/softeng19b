42651631af80f8180ec5a832b9be57fbfb33d785
a-panagiotic <58482696+a-panagiotic@users.noreply.github.com>
2020-03-02 23:31:24 +0200

    Update DayAheadTotalLoadForecastForSpecificMonth.java

--- back-end/src/main/java/gr/ntua/ece/softeng19b/api/resource/DayAheadTotalLoadForecastForSpecificMonth.java
+++ back-end/src/main/java/gr/ntua/ece/softeng19b/api/resource/DayAheadTotalLoadForecastForSpecificMonth.java
@@ -52,7 +52,7 @@ public class DayAheadTotalLoadForecastForSpecificMonth extends EnergyResource {
 
             if (result.isEmpty()) throw new ResourceException(Status.CLIENT_ERROR_FORBIDDEN);
 
-            return format.generateRepresentation(result);
+            return format.generateRepresentation3b(result);
         } catch (Exception e) {
             throw new ResourceException(Status.SERVER_ERROR_INTERNAL, e.getMessage(), e);
         }
