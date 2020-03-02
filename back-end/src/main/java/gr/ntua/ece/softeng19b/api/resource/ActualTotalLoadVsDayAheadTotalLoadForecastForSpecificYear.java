fc19fec64e57db41b80c1d1540261b670db5697d
a-panagiotic <58482696+a-panagiotic@users.noreply.github.com>
2020-03-02 23:32:46 +0200

    Update ActualTotalLoadVsDayAheadTotalLoadForecastForSpecificYear.java

--- back-end/src/main/java/gr/ntua/ece/softeng19b/api/resource/ActualTotalLoadVsDayAheadTotalLoadForecastForSpecificYear.java
+++ back-end/src/main/java/gr/ntua/ece/softeng19b/api/resource/ActualTotalLoadVsDayAheadTotalLoadForecastForSpecificYear.java
@@ -54,7 +54,7 @@ public class ActualTotalLoadVsDayAheadTotalLoadForecastForSpecificYear extends E
 
             if (result.isEmpty()) throw new ResourceException(Status.CLIENT_ERROR_FORBIDDEN);
 
-            return format.generateRepresentation(result);
+            return format.generateRepresentation4c(result);
         } catch (Exception e) {
             throw new ResourceException(Status.SERVER_ERROR_INTERNAL, e.getMessage(), e);
         }
