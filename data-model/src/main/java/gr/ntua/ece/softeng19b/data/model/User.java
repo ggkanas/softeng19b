d4af45ad93eb096efb067c4ae6dffe0bf30be8d1
a-panagiotic <58482696+a-panagiotic@users.noreply.github.com>
2020-03-02 12:48:51 +0200

    Update User.java

--- data-model/src/main/java/gr/ntua/ece/softeng19b/data/model/User.java
+++ data-model/src/main/java/gr/ntua/ece/softeng19b/data/model/User.java
@@ -1,20 +1,28 @@
 package gr.ntua.ece.softeng19b.data.model;
 
+import java.sql.Timestamp;
+
 public class User {
 
     private String username;
     private String password;
     private String email;
     private int requestsPerDayQuota; //negative values indicate no quota
+    private Timestamp period;
+    private int remainingRequests;
+    private String token;
 
     public User() {
         //Keep this for json encoding/decoding
     }
 
-    public User(String username, String email, int requestsPerDayQuota) {
+    public User(String username, String email, String password, int requestsPerDayQuota, Timestamp period, int remainingRequests) {
         this.username = username;
         this.email = email;
+        this.password = password;
         this.requestsPerDayQuota = requestsPerDayQuota;
+        this.period = period;
+        this.remainingRequests = remainingRequests;
     }
 
     public String getUsername() {
@@ -47,6 +55,30 @@ public class User {
 
     public int getRequestsPerDayQuota() {
         return requestsPerDayQuota;
+    }
+    
+    public void setRemainingRequests(int remainingRequests) {
+        this.remainingRequests = remainingRequests;
+    }
+
+    public int getRemainingRequests() {
+        return remainingRequests;
+    }
+    
+    public Timestamp getPeriod() {
+        return period;
+    }
+    
+    public void setPeriod(Timestamp period) {
+        this.period = period;
+    }
+    
+    public String getToken() {
+        return token;
+    }
+    
+    public void setToken(String token) {
+        this.token = token;
     }
 
 }
