f6686fc6ab168fb6f9e80f1ac99921216808d84a
a-panagiotic <58482696+a-panagiotic@users.noreply.github.com>
2020-03-02 23:29:25 +0200

    Update ActualTotalLoadForSpecificYear.java

--- back-end/src/main/java/gr/ntua/ece/softeng19b/api/resource/ActualTotalLoadForSpecificYear.java
+++ back-end/src/main/java/gr/ntua/ece/softeng19b/api/resource/ActualTotalLoadForSpecificYear.java
@@ -59,7 +59,7 @@ public class ActualTotalLoadForSpecificYear extends EnergyResource {
 
             if (result.isEmpty()) throw new ResourceException(Status.CLIENT_ERROR_FORBIDDEN);
 
-            return format.generateRepresentation(result);
+            return format.generateRepresentation1c(result);
         } catch (Exception e) {
             throw new ResourceException(Status.SERVER_ERROR_INTERNAL, e.getMessage(), e);
         }
