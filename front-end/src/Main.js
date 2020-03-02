88c7384bf1fa8405c01c9c16a060c87e99ac9150
ggkanas <giorgosf13@yahoo.gr>
2020-03-02 23:40:11 +0200

    Footer edits

--- front-end/src/Main.js
+++ front-end/src/Main.js
@@ -21,21 +21,7 @@ class Main extends Component {
                         Main page
                     </h1>
                 </div>
-                <div className="row">
-                    <form>
-                        <label htmlFor="inmode">Submit Mode</label><br/>
-                        <input id="inform" type="radio" name='inmode' value='form' defaultChecked onClick={()=>this.setState({inmode: "form"})} />
-                        <label htmlFor="inform">Form</label><br/>
-                        <input id="inmap" type="radio" name='inmode' value='map' onClick={()=>this.setState({inmode: "map"})} />
-                        <label htmlFor="inmap">Map</label><br/>
 
-                        <label htmlFor="outmode">Display Mode</label><br/>
-                        <input id="outtable" type="radio" name='outmode' value='table' defaultChecked onClick={()=>this.setState({outmode: "table"})} />
-                        <label htmlFor="outtable">Table</label><br/>
-                        <input id="outdiagram" type="radio" name='outmode' value='diagram' onClick={()=>this.setState({outmode: "diagram"})} />
-                        <label htmlFor="outdiagram">Diagram</label>
-                    </form>
-                </div>
                 <div className="row">
                     <Content inmode = {this.state.inmode} outmode = {this.state.outmode}/>
                 </div>
