afead6d957ecac61841c2b38a692bc03fb8924f3
a-panagiotic <58482696+a-panagiotic@users.noreply.github.com>
2020-03-02 23:28:46 +0200

    Update ActualTotalLoadForSpecificDate.java

--- back-end/src/main/java/gr/ntua/ece/softeng19b/api/resource/ActualTotalLoadForSpecificDate.java
+++ back-end/src/main/java/gr/ntua/ece/softeng19b/api/resource/ActualTotalLoadForSpecificDate.java
@@ -61,7 +61,7 @@ public class ActualTotalLoadForSpecificDate extends EnergyResource {
 
             if (result.isEmpty()) throw new ResourceException(Status.CLIENT_ERROR_FORBIDDEN);
 
-            return format.generateRepresentation(result);
+            return format.generateRepresentation1a(result);
         } catch (Exception e) {
             throw new ResourceException(Status.SERVER_ERROR_INTERNAL, e.getMessage(), e);
         }
