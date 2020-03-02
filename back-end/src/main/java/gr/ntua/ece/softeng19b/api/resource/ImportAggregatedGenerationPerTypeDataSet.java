ada5c1d9b0a99e48729e3fa7d34eb405b7c4fb45
a-panagiotic <58482696+a-panagiotic@users.noreply.github.com>
2020-03-02 12:28:18 +0200

    Add files via upload

---
+++ back-end/src/main/java/gr/ntua/ece/softeng19b/api/resource/ImportAggregatedGenerationPerTypeDataSet.java
@@ -0,0 +1,65 @@
+package gr.ntua.ece.softeng19b.api.resource;
+
+import gr.ntua.ece.softeng19b.conf.Configuration;
+import gr.ntua.ece.softeng19b.data.DataAccess;
+import gr.ntua.ece.softeng19b.data.model.User;
+import gr.ntua.ece.softeng19b.api.AuthenticationService;
+import gr.ntua.ece.softeng19b.api.representation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.restlet.data.Status;
+import org.restlet.representation.Representation;
+import org.restlet.resource.ResourceException;
+import org.restlet.util.Series;
+
+public class ImportAggregatedGenerationPerTypeDataSet extends EnergyResource {
+
+    private final DataAccess dataAccess = Configuration.getInstance().getDataAccess();
+
+    @Override
+    protected Representation post(Representation entity) throws ResourceException {
+
+        Form form = new form(entity);
+
+        Series headers = (Series) getRequestAttributes().get("org.restlet.http.headers");
+        String token = headers.getFirstValue("X-OBSERVATORY-AUTH"); //to be confirmed
+
+        if (!dataAccess.checkToken(token))
+            throw new ResourceException(Status.CLIENT_ERROR_UNAUTHORIZED);
+
+        //if (!dataAccess.checkToken(form.getFirstValue("X-OBSERVATORY-AUTH"))) //to be confirmed
+        //    throw new ResourceException(Status.CLIENT_ERROR_UNAUTHORIZED);
+
+        String csvFile = form.getFirstValue("file");
+        int totalRecordsInFile = 0;
+        int totalRecordsImported = 0;
+
+        BufferedReader br = null;
+        String line = "";
+        String csvSplitBy = ",";
+
+        br = new BufferedReader(new FileReader(csvFile));
+
+        line = br.readLine();
+        String[] dataLine = line.split(csvSplitBy);
+
+        while((line = br.readLine()) != null) {
+          totalRecordsInFile = totalRecordsInFile + 1;
+          String[] dataLine = line.split(csvSplitBy);
+
+          if(dataAccess.addActualAggregatedGenerationPerTypeRecord(dataLine) == 1) totalRecordsImported = totalRecordsImported + 1;
+          else break;
+        }
+
+        int totalRecordsInDatabase = dataAccess.getTotalRecordsInDatabase("ActualTotalLoad");
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("totalRecordsInFile", totalRecordsInFile);
+        map.put("totalRecordsImported", totalRecordsImported);
+        map.put("totalRecordsInDatabase", totalRecordsInDatabase);
+
+        return JsonMapRepresentation(map);
+    }
+}
