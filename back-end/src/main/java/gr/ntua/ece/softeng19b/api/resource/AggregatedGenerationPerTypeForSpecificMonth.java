49529d30abf6cfd2f164f8016a82feb670936353
a-panagiotic <58482696+a-panagiotic@users.noreply.github.com>
2020-03-02 19:47:24 +0200

    Update AggregatedGenerationPerTypeForSpecificMonth.java

--- back-end/src/main/java/gr/ntua/ece/softeng19b/api/resource/AggregatedGenerationPerTypeForSpecificMonth.java
+++ back-end/src/main/java/gr/ntua/ece/softeng19b/api/resource/AggregatedGenerationPerTypeForSpecificMonth.java
@@ -2,7 +2,7 @@ package gr.ntua.ece.softeng19b.api.resource;
 
 import gr.ntua.ece.softeng19b.api.Format;
 import gr.ntua.ece.softeng19b.conf.Configuration;
-import gr.ntua.ece.softeng19b.data.model.ATLRecordForSpecificDay;
+import gr.ntua.ece.softeng19b.data.model.AGPerTypeRecordForSpecificMonth;
 import gr.ntua.ece.softeng19b.data.DataAccess;
 import org.restlet.data.Status;
 import org.restlet.representation.Representation;
@@ -21,26 +21,40 @@ public class AggregatedGenerationPerTypeForSpecificMonth extends EnergyResource 
     @Override
     protected Representation get() throws ResourceException {
 
+        Series headers = (Series) getRequestAttributes().get("org.restlet.http.headers");
+        String token = headers.getFirstValue("X-OBSERVATORY-AUTH"); //to be confirmed
+
+        if (!dataAccess.checkToken(token))
+            throw new ResourceException(Status.CLIENT_ERROR_UNAUTHORIZED);
+
+        if (!dataAccess.hasRemaining(token))
+            throw new ResourceException(Status.CLIENT_ERROR_PAYMENT_REQUIRED);
+        dataAccess.changeRemaining(token);        
+        
         //Read the mandatory URI attributes
         String areaName = getMandatoryAttribute("AreaName", "AreaName is missing");
+        String productionType = getMandatoryAttribute("ProductionType", "ProductionType is missing");        
         String resolution = getMandatoryAttribute("Resolution", "Resolution is missing");
-        String productionType = getMandatoryAttribute("ProductionType", "ProductionType is missing");
+
         //Read the optional date attribute
         String dateParam = getAttributeDecoded("month");
 
-        YearMonth month = YearMonth.parse(dateParam);
+        YearMonth yearMonth = YearMonth.parse(dateParam);
 
         //Read the format query parameter
         Format format = parseFormat(getQueryValue("format"));
 
         try {
 
-            List<AGPTRecordForSpecificMonth> result = dataAccess.fetchActualDataLoadForSpecificMonth(
+            List<AGPerTypeRecordForSpecificMonth> result = dataAccess.fetchAggregatedGenerationPerTypeForSpecificMonth(
                     areaName,
                     resolution,
                     productionType,
-                    month
+                    yearMonth
             );
+            
+            if (result.isEmpty()) throw new ResourceException(Status.CLIENT_ERROR_FORBIDDEN);            
+            
             return format.generateRepresentation(result);
         } catch (Exception e) {
             throw new ResourceException(Status.SERVER_ERROR_INTERNAL, e.getMessage(), e);
